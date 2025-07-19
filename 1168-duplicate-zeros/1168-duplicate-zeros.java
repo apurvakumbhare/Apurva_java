a1edbac03452c251933d5629ec8d5bea0a4c7d63
Apurva Kumbhare <apurvakumbhare84@gmail.com>
2025-07-19 09:55:20 +0530

    Time: 14 ms (22.11%), Space: 45 MB (29.49%) - LeetHub

---
+++ 1168-duplicate-zeros/1168-duplicate-zeros.java
@@ -0,0 +1,15 @@
+class Solution {
+     public static void duplicateZeros(int[] arr) {
+
+        for (int i = 0; i < arr.length;) {
+            if(arr[i]==0){
+                for (int j = arr.length-1; j >i; j--) {
+                    arr[j]=arr[j-1];
+                }
+                i++;
+            }
+            i++;
+        }
+
+    }
+}
