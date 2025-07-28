2d5761e3e045171a265badd4355e78ad9baa0e96
Apurva Kumbhare <apurvakumbhare84@gmail.com>
2025-07-28 07:32:31 +0530

    Time: 0 ms (100%), Space: 42 MB (81.38%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length==0)return false;
+        int row=0;
+        int col=matrix[row].length-1;
+        
+        while(row<matrix.length && col>=0){
+            if(target==matrix[row][col])return true;
+                if(target>matrix[row][col]){
+                    row++;
+                }
+                else{
+                    col--;
+                }
+
+        }
+        return false;
+    }
+}
