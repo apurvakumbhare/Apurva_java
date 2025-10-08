6eabf37faa30c04d0c9a640440c09f711ad56d3e
Apurva Kumbhare <apurvakumbhare84@gmail.com>
2025-10-08 08:15:34 +0530

    Time: 215 ms (5.01%), Space: 92.2 MB (38.52%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,26 +1,26 @@
 import java.util.*;
 
 class RandomizedSet {
-    HashSet<Integer> set;
+   
     ArrayList<Integer> list; 
     Random rand;       
 
     public RandomizedSet() {
-        set = new HashSet<>();
+     
         list = new ArrayList<>();
         rand = new Random();
     }
 
     public boolean insert(int val) {
-        if (set.contains(val)) return false;
-        set.add(val);
+        if (list.contains(val)) return false;
+     
         list.add(val); 
         return true;
     }
 
     public boolean remove(int val) {
-        if (!set.contains(val)) return false;
-        set.remove(val);
+        if (!list.contains(val)) return false;
+   
         list.remove(Integer.valueOf(val));
         return true;
     }
