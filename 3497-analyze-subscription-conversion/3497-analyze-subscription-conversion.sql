d02e13f9f52e67f73e6eb752b815dcfee1451e37
Apurva Kumbhare <apurvakumbhare84@gmail.com>
2025-12-15 09:29:11 +0530

    Time: 309 ms (34.59%), Space: 0B (100%) - LeetHub

---
+++ 3497-analyze-subscription-conversion/3497-analyze-subscription-conversion.sql
@@ -0,0 +1,18 @@
+SELECT
+    user_id,
+    ROUND(
+        AVG(CASE
+                WHEN activity_type = 'free_trial' THEN activity_duration
+            END), 2
+    ) AS trial_avg_duration,
+    ROUND(
+        AVG(CASE
+                WHEN activity_type = 'paid' THEN activity_duration
+            END), 2
+    ) AS paid_avg_duration
+FROM UserActivity
+GROUP BY user_id
+HAVING
+    AVG(CASE WHEN activity_type = 'free_trial' THEN activity_duration END) IS NOT NULL
+AND AVG(CASE WHEN activity_type = 'paid' THEN activity_duration END) IS NOT NULL
+ORDER BY user_id;
