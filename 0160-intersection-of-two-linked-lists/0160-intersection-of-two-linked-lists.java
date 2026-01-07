d8ee2664812968c8e7d59d44043a28d576e215e7
Apurva Kumbhare <apurvakumbhare84@gmail.com>
2026-01-07 07:44:35 +0530

    Time: 2 ms (43.28%), Space: 53 MB (10.27%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -10,28 +10,42 @@
  * }
  */
 public class Solution {
-   public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if(headA==null ||headB==null)return null;
-       ArrayList<ListNode> listA=new ArrayList<>();
-        ArrayList<ListNode> listB=new ArrayList<>();
-        while(headA!=null){
-            listA.add(headA);
-            headA=headA.next;
-        }
-        while(headB!=null){
-            listB.add(headB);
-            headB=headB.next;
-        }
-        for (int i = 0; i < listA.size(); i++) {
-            for (int j = 0; j < listB.size(); j++) {
-                if(listA.get(i)==listB.get(j)){
-                    return listA.get(i);
+//    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//         if(headA==null ||headB==null)return null;
+//        ArrayList<ListNode> listA=new ArrayList<>();
+//         ArrayList<ListNode> listB=new ArrayList<>();
+//         while(headA!=null){
+//             listA.add(headA);
+//             headA=headA.next;
+//         }
+//         while(headB!=null){
+//             listB.add(headB);
+//             headB=headB.next;
+//         }
+//         for (int i = 0; i < listA.size(); i++) {
+//             for (int j = 0; j < listB.size(); j++) {
+//                 if(listA.get(i)==listB.get(j)){
+//                     return listA.get(i);
                     
-                }
-            }
-        }
+//                 }
+//             }
+//         }
        
-        return null;
+//         return null;
         
+//     }
+    //optimal
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a != b) {
+            a = (a == null) ? headB : a.next;
+            b = (b == null) ? headA : b.next;
+        }
+
+        return a; 
     }
 }
