43d793b3abc37dba2474ec42f097ceabadc7b212
Apurva Kumbhare <apurvakumbhare84@gmail.com>
2026-01-16 21:50:26 +0530

    Time: 5 ms (39.91%), Space: 47.3 MB (48.75%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int perimeter=0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j]==1){
+                    int toAdd=4;
+                    if(i>0 && grid[i-1][j]==1) toAdd--;
+                    if(i< grid.length-1 && grid[i+1][j]==1) toAdd--;
+                    if(j>0 && grid[i][j-1]==1) toAdd--;
+                    if(j< grid[0].length-1 && grid[i][j+1]==1) toAdd--;
+                    perimeter+=toAdd;
+                }
+            }
+        }
+        return perimeter;
+    }
+}
